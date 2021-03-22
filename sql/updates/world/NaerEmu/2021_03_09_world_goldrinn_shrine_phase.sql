aa4ab12bb257474fe1bacfc682271b6c9c38915b
rinsewind <rincewind.zonelimit@gmail.com>
2021-03-22 22:54:19 +0100

    Mount Hyjal: Shrine of Goldrinn phase change

---
+++ sql/updates/world/NaerEmu/2021_03_09_world_goldrinn_shrine_phase.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_area` where `spell`='74091';
+INSERT INTO `spell_area` (`spell`,`area`,`quest_start`,`quest_end`,`aura_spell`,`racemask`,`gender`,`flags`,`quest_start_status`,`quest_end_status`) VALUES
+('74091','4979','0','25272','0','946','2','3','64','11'),
+('74091','4979','0','25273','0','2098253','2','3','64','11');
